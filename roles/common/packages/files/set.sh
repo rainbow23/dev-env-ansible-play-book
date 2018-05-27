a0cd37c5c2beb2b338c832573098f7127d7ac115
rainbow <info@rainbow23.jp>
2018-05-27 20:32:24 +0900

    整理

---
+++ roles/common/packages/files/set.sh
@@ -0,0 +1,31 @@
+# autojump
+[[ -s $HOME/.autojump/etc/profile.d/autojump.sh ]] && source $HOME/.autojump/etc/profile.d/autojump.sh
+
+# fzf
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+# Enhancd
+if [ -d $HOME/.enhancd ] ; then
+    source $HOME/.enhancd/init.sh
+fi
+
+# anyenv
+if [ -d $HOME/.anyenv ] ; then
+    export PATH="$HOME/.anyenv/bin:$PATH"
+    eval "$(anyenv init -)"
+fi
+
+
+# goenv
+if [ -d $HOME/.anyenv/envs/goenv ] ; then
+    export GOPATH=$HOME/go
+    export GOBIN=$GOPATH/bin
+    eval "$(goenv init -)"
+fi
+
+# pyenv
+if [ -d $HOME/.anyenv/envs/pyenv ] ; then
+    export PATH="$HOME/.anyenv/envs/pyenv/bin:$PATH"
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+fi
